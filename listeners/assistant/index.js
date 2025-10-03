e2065063420ff22416884fce5719dd9ab9a189ac
Michael Brooks <mbrooks@slack-corp.com>
2025-10-03 09:31:18 -0700

    refactor: separate assistant/ into individual files named after events

--- listeners/assistant/index.js
+++ listeners/assistant/index.js
@@ -1,11 +1,13 @@
 import { Assistant } from '@slack/bolt';
-import { threadContextChanged, threadStarted, userMessage } from './assistant.js';
+import { assistantThreadContextChanged } from './assistant_thread_context_changed.js';
+import { assistantThreadStarted } from './assistant_thread_started.js';
+import { message } from './message.js';
 
 export const register = (app) => {
   const assistant = new Assistant({
-    threadStarted: threadStarted,
-    threadContextChanged: threadContextChanged,
-    userMessage: userMessage,
+    threadStarted: assistantThreadStarted,
+    threadContextChanged: assistantThreadContextChanged,
+    userMessage: message,
   });
 
   app.assistant(assistant);
--- listeners/assistant/message.js
+++ listeners/assistant/message.js
@@ -2,95 +2,11 @@ import { DEFAULT_SYSTEM_CONTENT, openai } from '../../ai/index.js';
 import { feedbackBlock } from '../views/feedback_block.js';
 
 /**
- * `assistant_thread_started` is sent when a user opens the Assistant container.
- * This can happen via DM with the app or as a side-container within a channel.
- *
- * @see {@link https://docs.slack.dev/reference/events/assistant_thread_started}
- */
-export const threadStarted = async ({ event, logger, say, setSuggestedPrompts, saveThreadContext }) => {
-  const { context } = event.assistant_thread;
-
-  try {
-    /**
-     * Since context is not sent along with individual user messages, it's necessary to keep
-     * track of the context of the conversation to better assist the user. Sending an initial
-     * message to the user with context metadata facilitates this, and allows us to update it
-     * whenever the user changes context (via the `assistant_thread_context_changed` event).
-     * The `say` utility sends this metadata along automatically behind the scenes.
-     * !! Please note: this is only intended for development and demonstrative purposes.
-     */
-    await say('Hi, how can I help?');
-
-    await saveThreadContext();
-
-    /**
-     * Provide the user up to 4 optional, preset prompts to choose from.
-     *
-     * The first `title` prop is an optional label above the prompts that
-     * defaults to 'Try these prompts:' if not provided.
-     *
-     * @see {@link https://docs.slack.dev/reference/methods/assistant.threads.setSuggestedPrompts}
-     */
-    if (!context.channel_id) {
-      await setSuggestedPrompts({
-        title: 'Start with this suggested prompt:',
-        prompts: [
-          {
-            title: 'This is a suggested prompt',
-            message:
-              'When a user clicks a prompt, the resulting prompt message text ' +
-              'can be passed directly to your LLM for processing.\n\n' +
-              'Assistant, please create some helpful prompts I can provide to ' +
-              'my users.',
-          },
-        ],
-      });
-    }
-
-    /**
-     * If the user opens the Assistant container in a channel, additional
-     * context is available. This can be used to provide conditional prompts
-     * that only make sense to appear in that context.
-     */
-    if (context.channel_id) {
-      await setSuggestedPrompts({
-        title: 'Perform an action based on the channel',
-        prompts: [
-          {
-            title: 'Summarize channel',
-            message: 'Assistant, please summarize the activity in this channel!',
-          },
-        ],
-      });
-    }
-  } catch (e) {
-    logger.error(e);
-  }
-};
-
-/**
- * `assistant_thread_context_changed` is sent when a user switches channels
- * while the Assistant container is open. If `threadContextChanged` is not
- * provided, context will be saved using the AssistantContextStore's `save`
- * method (either the DefaultAssistantContextStore or custom, if provided).
- *
- * @see {@link https://docs.slack.dev/reference/events/assistant_thread_context_changed}
- */
-export const threadContextChanged = async ({ logger, saveThreadContext }) => {
-  // const { channel_id, thread_ts, context: assistantContext } = event.assistant_thread;
-  try {
-    await saveThreadContext();
-  } catch (e) {
-    logger.error(e);
-  }
-};
-
-/**
- * Messages sent from the user to the Assistant are handled in this listener.
+ * `message` event is sent when the user direct messages the app in a DM or Assistant container.
  *
  * @see {@link https://docs.slack.dev/reference/events/message}
  */
-export const userMessage = async ({ client, context, logger, message, getThreadContext, say, setTitle, setStatus }) => {
+export const message = async ({ client, context, logger, message, getThreadContext, say, setTitle, setStatus }) => {
   /**
    * Messages sent to the Assistant can have a specific message subtype.
    *
