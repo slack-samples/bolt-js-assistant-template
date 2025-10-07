c53c3935cee83651b2956a292cfe66b733e43f04
Ale Mercado <maria.mercado@slack-corp.com>
2025-10-07 16:44:17 -0400

    fix: linter errors

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import 'dotenv/config';
 import { App, LogLevel } from '@slack/bolt';
 import { registerListeners } from './listeners/index.js';
 
-
 // Initialize the Bolt app
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
