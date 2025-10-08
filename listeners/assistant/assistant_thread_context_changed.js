f556b17ec7d03fc3068dbdf085e4d6d5a43c0e54
Eden Zimbelman <eden.zimbelman@salesforce.com>
2025-10-08 14:35:39 -0700

    feat: filter feedback button actions with stronger types (#75)
    * feat: add support for OpenAI API (#61)
    
    * feat: add support for OpenAI API
    
    * feat: add streaming methods to node assistant (#64)
    
    * feat: add chat.{appendStream|startStream|stopStream} methods
    
    * build: linter errors
    
    * feat: add streaming helpers
    
    * build: bump slack bolt/webapi to beta.4
    
    * feat: stream openai responses
    
    * docs: remove huggingface references
    
    * build: update package-lock.json
    
    * feat: update setStatus to include a status and loading_messages (#65)
    
    * feat: update setStatus to include a status and loading_messages
    
    * fix: linting errors
    
    * chore: order package dependencies
    
    ---------
    
    Co-authored-by: Eden Zimbelman <eden.zimbelman@salesforce.com>
    
    * feat: feedback button (#73)
    
    * feat: feedback button
    
    * chore: revert package-lock.json to match package.json
    
    * chore: refactor to declare constants at top and add comments
    
    * chore: remove unused return statement
    
    * docs: add comment above the feedback action handler
    
    * docs: remove unnecessary link
    
    * Update app.js
    
    Co-authored-by: Eden Zimbelman <eden.zimbelman@salesforce.com>
    
    * Update app.js
    
    Co-authored-by: Eden Zimbelman <eden.zimbelman@salesforce.com>
    
    ---------
    
    Co-authored-by: Michael Brooks <mbrooks@slack-corp.com>
    Co-authored-by: Eden Zimbelman <eden.zimbelman@salesforce.com>
    
    * feat: add app_mention event support (#72)
    
    * feat: add app_mention event support
    
    * feat: add recipient_team_id and recipient_user_id to userMessage
    
    * feat: add recipient_team_id and recipient_user_id to summarize channel
    
    * refactor: move app_mention handler to top of file
    
    * feat: filter feedback button actions with stronger types
    
    * chore: lint
    
    * refactor: organize app into the common template directory structure (#74)
    
    Co-authored-by: Michael Brooks <mbrooks@slack-corp.com>
    
    * chore: update to the latest beta release
    
    * fix: jsdoc exports for confident testing
    
    * feat: filter out non feedback_buttons actions
    
    * feat: shorten app display name from assistant to agent (#76)
    
    * docs: update readme
    
    * refactor: tweak wording to be ai-agent and AI Agent
    
    ---------
    
    Co-authored-by: Michael Brooks <mbrooks@slack-corp.com>
    
    * revert: undo a merge conflict?
    
    ---------
    
    Co-authored-by: Maria Alejandra <104795114+srtaalej@users.noreply.github.com>
    Co-authored-by: Michael Brooks <mbrooks@slack-corp.com>

--- listeners/assistant/assistant_thread_context_changed.js
+++ listeners/assistant/assistant_thread_context_changed.js
@@ -1,8 +1,12 @@
 /**
- * `assistant_thread_context_changed` is sent when a user switches channels
- * while the Assistant container is open. If `threadContextChanged` is not
- * provided, context will be saved using the AssistantContextStore's `save`
+ * The `assistant_thread_context_changed` event is sent when a user switches
+ * channels while the Assistant container is open. If `threadContextChanged` is
+ * not provided, context will be saved using the AssistantContextStore's `save`
  * method (either the DefaultAssistantContextStore or custom, if provided).
+ *
+ * @param {Object} params
+ * @param {import("@slack/bolt").Logger} params.logger - Logger instance.
+ * @param {Function} params.saveThreadContext - Function to save thread context.
  *
  * @see {@link https://docs.slack.dev/reference/events/assistant_thread_context_changed}
  */
